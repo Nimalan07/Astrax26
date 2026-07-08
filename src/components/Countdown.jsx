c49360b44d2c5b73f29e30c43ab6787e79c70c81
Nimalan Mani <mnimalanmani@gmail.com>
2026-07-08 19:47:58 +0530

    files

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "../styles/Countdown.css";
 
-const EVENT_DATE = new Date("2026-07-20T09:00:00");
+const EVENT_DATE = new Date("2026-07-22T09:00:00");
 
 function Countdown() {
 
